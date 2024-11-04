efe428d22cca64198b6335c5bc47607e5fead16e
k1rel <kiril.gerasimovski@gmail.com>
2024-11-04 22:29:23 +0100

    added all of the attributes

--- main.py
+++ main.py
@@ -1,61 +1,30 @@
+from utils.extract import extract_html
 from selectolax.parser import HTMLParser
-from time import sleep
-from playwright.sync_api import sync_playwright
+
 
 URL = "https://store.steampowered.com/specials"
 
 if __name__ == "__main__":
-    with sync_playwright() as pw:
-        browser = pw.chromium.launch(headless=True)
-        page = browser.new_page()
-        page.goto(URL)
+   
+    tree = HTMLParser(extract_html(URL))
 
-       
-        page.wait_for_load_state("load")
+    divs = tree.css('div[class*="gASJ2lL_xmVNuZkWGvrWg"]')
 
+    for div in divs:
+        parent_for_date = div.css_first("div.StoreSaleWidgetTitle").parent.parent
+        attrs = {
+            "title" : div.css_first("div[class*='StoreSaleWidgetTitle']").text(),
+            "img_url": div.css_first("div[class*=CapsuleImageCtn] img").attributes.get("src"),
+            "tags" : [i.text() for i in div.css("a[class*='WidgetTag']")[:5]],
+            "release_date": parent_for_date.css_first("div + div > div + div > div").text(),
+            "review_score": div.css("a[class*='ReviewScore'] > div  > div")[0].text(),
+            "reviews_count": div.css("a[class*='ReviewScore'] > div  > div")[2].text(),
+            "original_price": div.css("div[class*=StoreSalePriceWidgetContainer] > div + div > div")[0].text(),
+            "discount_price": div.css("div[class*=StoreSalePriceWidgetContainer] > div + div > div")[1].text()
 
-        
-        last_position = 0
-        reached_end = False
-
-        while not reached_end:
-           
-            page.evaluate("window.scrollBy(0, 800)")
-            sleep(2) 
-
-         
-            current_position = page.evaluate("() => window.scrollY")
-            page_height = page.evaluate("() => document.body.scrollHeight")
-
-            
-            if current_position + page.viewport_size['height'] >= page_height:
-                reached_end = True  
-            
-        
-            if current_position == last_position:
-                reached_end = True  
-            last_position = current_position
+        }
 
         
+        print(attrs)
 
 
-        # page.screenshot(path="steam.png", full_page=True)
-
-        html = page.inner_html("body")
-        tree = HTMLParser(html)
-
-        divs = tree.css('div[class*="gASJ2lL_xmVNuZkWGvrWg"]')
-
-        for div in divs:
-          
-        
-        
-                
-            attrs = {
-                "title" : div.css_first("div[class*='StoreSaleWidgetTitle']").text(),
-                "img_url": div.css_first("div[class*=CapsuleImageCtn] img").attributes.get("src"),
-                "tags" : [i.text() for i in div.css("a[class*='WidgetTag']")[:5]]
-            }
-            print(attrs)
-
-        browser.close()
---
+++ utils/extract.py
@@ -0,0 +1,40 @@
+from time import sleep
+from playwright.sync_api import sync_playwright 
+
+
+def extract_html(URL):
+
+    with sync_playwright() as pw:
+            browser = pw.chromium.launch(headless=True)
+            page = browser.new_page()
+            page.goto(URL)
+
+        
+            page.wait_for_load_state("load")
+
+
+            
+            last_position = 0
+            reached_end = False
+
+            while not reached_end:
+            
+                page.evaluate("window.scrollBy(0, 800)")
+                sleep(2) 
+
+            
+                current_position = page.evaluate("() => window.scrollY")
+                page_height = page.evaluate("() => document.body.scrollHeight")
+
+                
+                if current_position + page.viewport_size['height'] >= page_height:
+                    reached_end = True  
+                
+            
+                if current_position == last_position:
+                    reached_end = True  
+                last_position = current_position
+            html = page.inner_html("body")
+            browser.close() 
+            return html
+                   
